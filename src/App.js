156008b528b1c9b25bd3f69e69f13f92db41688a
Marcin <m.niemirski@time4.digital>
2021-04-22 00:24:04 +0200

    Add Mapbox and NASA apis, create basic working logic

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import { makeStyles } from '@material-ui/core/styles';
+import 'promise-polyfill/src/polyfill';
 
-import {
-  Grid,
-  Paper,
-  Container,
-  Typography,
-  FormControl,
-  Input,
-  InputLabel,
-} from '@material-ui/core';
-
+import { Grid, Paper, Container, Typography } from '@material-ui/core';
+import SearchInput from './components/SearchInput';
+const token =
+  'pk.eyJ1IjoibmllbWlyIiwiYSI6ImNrbnJ0c3UwNzA2dG8ycm83NW5sdjd6eTQifQ.X7G46SsLiU90RrDMmwXSHQ';
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -57,10 +52,7 @@ const App = () => {
               </Typography>
             </Grid>
             <Grid container justify='center' spacing={2}>
-              <FormControl>
-                <InputLabel htmlFor='my-input'>Location</InputLabel>
-                <Input id='my-input' aria-describedby='my-helper-text' />
-              </FormControl>
+              <SearchInput />
             </Grid>
           </Grid>
         </Grid>
