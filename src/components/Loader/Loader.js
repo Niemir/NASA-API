ebf9f5b150b1480ba0b09c340dde0071063aa414
Marcin <m.niemirski@time4.digital>
2021-04-22 21:16:25 +0200

    Add loader before image fetch

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Spinner } from './Loader.styles';
+
+const Loader = () => (
+  <Spinner className='loader'>
+    <div className='lds-roller'>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </Spinner>
+);
+export default Loader;
